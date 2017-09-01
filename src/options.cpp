6ded957550a0b2d8c31d8df86cb214ffa48b49eb
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2017-09-01 11:54:17 +0300

    core: Relicense remaining files to LGPLv2.1+
    The log.* files were originally MIT so it's fine to relicense to
    LGPLv2.1+
    
    The options.* files were originally GPLv3+ but have been rewritten to
    such an extent that they are effectively new files, so relicensing
    should be fine, too.

--- src/options.cpp
+++ src/options.cpp
@@ -1,21 +1,20 @@
 /*
- * Copyright © 2011 Linaro Ltd.
  * Copyright © 2017 Collabora Ltd.
  *
  * This file is part of vkmark.
  *
- * vkmark is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * vkmark is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * vkmark is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with vkmark.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with vkmark. If not, see <http://www.gnu.org/licenses/>.
  *
  * Authors:
  *   Alexandros Frantzis <alexandros.frantzis@collabora.com>
