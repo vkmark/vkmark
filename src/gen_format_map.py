b9ebcc189c949ae01f133ef221c0b2384d5ea3f8
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2025-01-07 14:29:17 +0200

    build: Generate format names map from vk.xml
    Get the list of available formats from the vk.xml spec file, instead of
    trying to extract them from the vulkan-hpp header files.

--- src/gen_format_map.py
+++ src/gen_format_map.py
@@ -19,23 +19,25 @@
 #   Alexandros Frantzis <alexandros.frantzis@collabora.com>
 
 import sys
+import xml.etree.ElementTree as ET
 
 infile = sys.argv[1]
 outfile = sys.argv[2]
 
-in_formats = False
+def vk_fmt_to_hpp(fmt):
+    fmt = fmt.replace("VK_FORMAT_", "")
+    ret = ""
+    last_was_alpha = False
+    for c in fmt:
+        ret += c.lower() if last_was_alpha else c
+        last_was_alpha = c.isalpha()
+    return ret.replace("_", "")
 
 with open(infile, 'r') as inf, open(outfile, 'w') as outf:
     print("#include <unordered_map>", file=outf);
     print("static std::unordered_map<std::string, vk::Format> format_map = {", file=outf)
-    for line in inf:
-        line = line.strip()
-        if in_formats:
-            if line.startswith('e'):
-                enum = line.split('=')[0].strip()
-                print('    {"%s", vk::Format::%s},' % (enum[1:].upper(), enum), file=outf)
-            if line.endswith('};'): 
-                break
-        elif line.startswith('enum class Format'):
-            in_formats = True
-    print("};", file=outf)
+    vk_xml = ET.fromstring(inf.read())
+    for enum in vk_xml.findall(".//enums[@name='VkFormat']/enum"):
+        fmt = vk_fmt_to_hpp(enum.get("name"))
+        print('    {"%s", vk::Format::e%s},' % (fmt.upper(), fmt), file=outf)
+    print("};", file=outf);
